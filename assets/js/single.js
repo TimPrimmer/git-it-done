3b4fb557cc84cb36c491bd5ed6aad6f0c301fef2
Tim Primmer <timprimmer@gmail.com>
2021-06-10 18:16:47 -0500

    added support for viewing issues on another page and error checking

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,19 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+  if (repoName) {
+    repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+  }
+  else{
+    document.location.replace("./index.html");
+  }
+}
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -17,7 +31,7 @@ var getRepoIssues = function (repo) {
       });
     }
     else {
-      alert("There was a problem with your request!");
+      document.location.replace("./index.html");
     }
   });
 };
@@ -52,7 +66,7 @@ var displayIssues = function (issues) {
   }
 };
 
-var displayWarning = function(repo) {
+var displayWarning = function (repo) {
   // add text to warning container
   limitWarningEl.textContent = "To see more than 30 issues, visit ";
   var linkEl = document.createElement("a");
@@ -64,4 +78,4 @@ var displayWarning = function(repo) {
   limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("expressjs/express");
+getRepoName();
